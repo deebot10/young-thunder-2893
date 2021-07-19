b3a59d265fe469c2fa7efcefa8abcd53e53c14f3
deebot10 <dee1jhill@gmail.com>
2021-07-19 11:02:08 -0600

    Test actor class methods

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe Actor do
     it { should have_many(:actor_movies)}
     it { should have_many(:movies).through(:actor_movies)}    
   end    
+
+  before(:each) do
+    @studio = Studio.create!(name: 'Universal Studios', location: 'Hollywood') 
+
+    @movie = @studio.movies.create!(title: 'Sound of Metal', creation_year: 2020, genre: 'Action')
+    @movie_1 = @studio.movies.create!(title: 'The Dark Knight', creation_year: 2010)   
+
+    @actor_1 = @movie.actors.create!(name: 'John Boyega', age: 27)
+    @actor_2 = @movie.actors.create!(name: 'Samuel L Jackson', age: 81)
+    @actor_3 = @movie.actors.create!(name: 'Lakeith Stanfield', age: 33)
+    @actor_4 = @movie.actors.create!(name: "Lupita Nyong'o", age: 34) 
+  end
+
+  describe 'class methods' do
+    it 'orders actors by age' do
+
+      expect(Actor.actor_age).to eq([@actor_1, @actor_3, @actor_4, @actor_2])
+    end
+
+    it 'displays the average age of actors' do
+
+      expect(Actor.average_age).to eq(0.4375e2)
+    end
+  end
 end
