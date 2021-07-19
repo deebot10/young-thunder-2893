e173f8f1a3b0463e3fae8ee5d01249351c9ac62f
deebot10 <dee1jhill@gmail.com>
2021-07-19 10:48:03 -0600

    Create test for Stroy3

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Movie Show' do
     @studio = Studio.create!(name: 'Universal Studios', location: 'Hollywood') 
 
     @movie = @studio.movies.create!(title: 'Sound of Metal', creation_year: 2020, genre: 'Action')
-    # @movie_1 = @studio.movies.create!(title: 'The Dark Knight', creation_year: 2010)   
+    @movie_1 = @studio.movies.create!(title: 'The Dark Knight', creation_year: 2010)   
 
     @actor_1 = @movie.actors.create!(name: 'John Boyega', age: 27)
     @actor_2 = @movie.actors.create!(name: 'Samuel L Jackson', age: 81)
@@ -15,14 +15,7 @@ RSpec.describe 'Movie Show' do
     visit "/movies/#{@movie.id}"
   end 
   
-    # Story 2
-    # Movie Show
-    
-    # As a user,
-    # When I visit a movie's show page.
-    # I see the movie's title, creation year, and genre,
-    # and a list of all its actors from youngest to oldest.
-    # And I see the average age of all of the movie's actors
+  # Story 2
   it 'shows a movie and its attributes' do
 
     expect(page).to have_content(@movie.title)
@@ -38,6 +31,28 @@ RSpec.describe 'Movie Show' do
     expect(@actor_2.name).to_not appear_before(@actor_1.name)
     
     expect(page).to have_content('Average Age:43.75')
-    save_and_open_page
+  end
+  
+    # Story 3
+    # Add an Actor to a Movie
+    
+    # As a user,
+    # When I visit a movie show page,
+    # I do not see any actors listed that are not part of the movie
+    # And I see a form to add an actor to this movie
+    # When I fill in the form with the name of an actor that exists in the database
+    # And I click submit
+    # Then I am redirected back to that movie's show page
+    # And I see the actor's name is now listed
+    # (You do not have to test for a sad path, for example if the name submitted is not an existing actor)
+  it 'has a form to add a actor to a movie' do
+    visit "/movies/#{@movie_1.id}"
+
+    expect(page).to have_content(@actor_1.name)
+    
+    fill_in 'Actor Name', with: 'John Boyega'
+    click_on('Submit')
+    
+    expect(page).to have_content(@actor_1.name)
   end
 end
